fb27748de20324d12ddd6c421696fbf7d70f531e
Note <85052819+4086@users.noreply.github.com>
2022-09-26 16:53:13 +0900

    Add files via upload

---
+++ room/notiaddex/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.doublenotify
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.widget.EditText
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val et = findViewById<EditText>(R.id.et_id)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            showNotification()
+        }
+
+        val notify = findViewById<Button>(R.id.notify)
+        notify.setOnClickListener {
+            var txt = et.text.toString()
+            showNotification2(txt)
+        }
+
+        requestSinglePermission(Manifest.permission.POST_NOTIFICATIONS)
+
+        createNotificationChannel()
+        createNotificationChannel2()
+    }
+
+    private val channelID = "default"
+    private val channelID2 = "ad"
+    var count = 1
+
+    private fun showNotification() {
+        val builder = NotificationCompat.Builder(this, channelID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Notification Lab.")
+            .setContentText("Notification # ${count++}")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        NotificationManagerCompat.from(this)
+            .notify(1, builder.build())
+    }
+
+    private fun showNotification2(txt: String) {
+        val builder = NotificationCompat.Builder(this, channelID2)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("Notification Lab2.")
+            .setContentText(txt)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        NotificationManagerCompat.from(this)
+            .notify(2, builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            channelID, "default channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.description = "description text of this channel."
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createNotificationChannel2() {
+        val channel = NotificationChannel(
+            channelID2, "ad channel",
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        channel.description = "description text of this channel."
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun requestSinglePermission(permission: String) {
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED)
+            return
+
+        val requestPermLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it == false) { // permission is not granted!
+                AlertDialog.Builder(this).apply {
+                    setTitle("Warning")
+                    setMessage("Warning")
+                }.show()
+            }
+        }
+
+        if (shouldShowRequestPermissionRationale(permission)) {
+            // you should explain the reason why this app needs the permission.
+            AlertDialog.Builder(this).apply {
+                setTitle("Reason")
+                setMessage("Reason")
+                setPositiveButton("Allow") { _, _ -> requestPermLauncher.launch(permission) }
+                setNegativeButton("Deny") { _, _ -> }
+            }.show()
+        } else {
+            // should be called in onCreate()
+            requestPermLauncher.launch(permission)
+        }
+    }
+}
