71d0d26fdc22d96b87cc7879c9a6273107e9f42a
Note <85052819+4086@users.noreply.github.com>
2022-09-28 01:19:51 +0900

    Add files via upload

---
+++ transition/transitionaddex/MyViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.transitionaddex
+
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    var str1: String = "Name"
+    var str2: String = "Name"
+    var checked: Int = 0
+}
