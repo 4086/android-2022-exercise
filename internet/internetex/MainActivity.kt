d7f6cb10117109137da7d1c82a61457002e5c8b1
Note <85052819+4086@users.noreply.github.com>
2022-10-01 21:18:46 +0900

    Add files via upload

---
+++ internet/internetex/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.internetex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel : MyViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MyViewModel::class.java]
+
+        findViewById<Button>(R.id.query).setOnClickListener {
+            if (findViewById<EditText>(R.id.username).text.isNotEmpty()) {
+                viewModel.uname = findViewById<EditText>(R.id.username).text.toString()
+                viewModel.response.observe(this) {
+                    findViewById<TextView>(R.id.textView).text = it
+                }
+            }
+        }
+    }
+}
